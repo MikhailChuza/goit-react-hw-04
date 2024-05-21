030f7f9cab8d40fc15b37ce2e0ad7374bba276bc
MikhailChuza <148324632+MikhailChuza@users.noreply.github.com>
2024-05-21 17:27:44 +0300

    Access Key

--- src/App.jsx
+++ src/App.jsx
@@ -3,3 +3,9 @@ import { useState } from "react";
 import "./App.css";
 
 export default App;
+
+// Application ID    614696
+
+// Access Key   4DRL2zuvY1q - KRaEaDp3GrlWHWl2 - TXIdUQy05dZwR8
+
+// Secret key   l8c9-nQ8aGBv6eHXuJXYTqvSNRG0vrXB3jYWsRLNTPY
